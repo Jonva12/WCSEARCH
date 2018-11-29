89ad3fc52035798704883ce9d4039f82b68ff58d
yaimsc <yaimsc@gmail.com>
2018-11-29 11:33:16 +0100

    relation usuario

--- app/Usuario.php
+++ app/Usuario.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Comentario;
+use App\Aseo;
 
 class Usuario extends Model
 {
-    //
+    public function comentario(){
+      return $this->hasMany('App\Comentario');
+    }
+
+    public function valoracion(){
+      return $this->belongsToMany('App\Comentario')->withPivot('puntuacion');
+    }
+
+    public function aseo(){
+      return $this->hasMany('App\Aseo');
+    }
 }
