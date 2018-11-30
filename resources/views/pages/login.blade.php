f3b5491e4e990eb20ff1d3c5887d2856ddcf7886
jonva12 <jonva12@hotmail.com>
2018-11-30 10:45:20 +0100

    fondo login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,1 +1,20 @@
- 
+ @extends('layout.login')
+
+ @section('title', 'Login WCSearch')
+
+  @section('content')
+    <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-7 mx-auto">
+              <img class="img-fluid" id="header" src="img/iphone2.png" alt="WCSearch" >
+          </div>
+          <div class="col-lg-5 mx-auto"><hr>
+            <h1 id="welcome"><strong>ENCUENTRA LOS ASEOS PUBLICOS DE MANERA RAPIDA Y SENCILLA</strong></h1>
+            <br/>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Registrate!!!</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  @endsection
