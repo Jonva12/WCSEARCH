973aa3772a0b10a8684fc957612e0e27e764b2f0
jonva12 <jonva12@hotmail.com>
2019-01-07 10:27:12 +0100

    Cambios vista usuario

--- resources/views/includes/user/aside.blade.php
+++ resources/views/includes/user/aside.blade.php
@@ -2,7 +2,10 @@
 	<a href="{{ route('home') }}">
 		<li><i class="fas fa-home"></i> Inicio</li>
 	</a>
-	<a href="{{ route('usuario.perfil') }}">
-		<li><i class="fas fa-users"></i> Ajustes</li>
+	<a href="{{ route('usuario.perfil',$usuario->id) }}">
+		<li><i class="fas fa-users"></i> Perfil</li>
+	</a>
+	<a href="">
+		<li><i class="fa fa-bell"></i> Notificaciones</li>
 	</a>
 </ul>
