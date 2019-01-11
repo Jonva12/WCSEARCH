f3e2b5981799d5bd437ded597022171454917ed5
jonva12 <jonva12@hotmail.com>
2019-01-11 10:08:21 +0100

    prueba redireccion

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -36,6 +36,6 @@ class VerificationController extends Controller
     {
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
-        $this->middleware('throttle:6,1')->only('verify', 'verification.resend');
+        $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
