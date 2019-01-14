28b6015cc21d9958a5863c8257b75cd9cee4427d
yaimsc <yaimsc@gmail.com>
2019-01-14 11:03:37 +0100

    alert

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,9 +4,7 @@
 
 @section('content')
   <div id="mapid"></div>
-  <div class="navbar column">
-			<a class="nav-link" class="link" href="/createWC">Create WC</a>
-	</div>
+			
   <!-- <button id="geolocate">Geo</button> -->
   @include('includes.geoscripts')
   <script src="js/map.js"></script>
