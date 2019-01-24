4f88e85cfa8954587ac02aaa7afc5de2c7ed21b3
yaimsc <yaimsc@gmail.com>
2019-01-24 13:26:40 +0100

    alerts

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,80 +1,68 @@
 @extends('layout.app')
-  @section('title','WCSEARCH')
-  @section('content')
-  <style type="text/css">
-    aside {
-      box-shadow: 10px 0px 10px 1px #aaaaaa;
-      padding: 0px !important;
-    }
-    aside img{
-      width: 100%;
-      height: 300px;
-    }
-    aside .general{
-      color: white;
-      background-color: #28a745;
-      padding: 5px;
-    }
-    aside .info{
-      padding-left: 15px;
-    }
-    #mapid{
-      height: 800px;
-      width: 100%;
-      margin-top: -20px;
-    }
-    .fa-arrow-left{
-      color: green;
-      padding: 10px;
-    }
-    #error_zoom{
-      color: white;
-      width: 100%;
-      height: 50px;
-      text-align: center;
-      background-color: #778899;
-      margin-top: 0px;
-      margin-left: auto;
-      margin-right: auto;
-      display: hidden;
-    }
-    .alert{
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      margin-left: 5%;
-      margin-right: 5%;
-      border: 0.5px solid #857B26;
-      box-shadow: 1px 1.5px 2px 1px #857B26; 
-    }
-    .fa-times{
-      font-size: 2em;
-    }
-  </style>
+@section('title','WCSEARCH')
+@section('content')
+<style type="text/css">
+aside {
+  box-shadow: 10px 0px 10px 1px #aaaaaa;
+  padding: 0px !important;
+}
+aside img{
+  width: 100%;
+  height: 300px;
+}
+aside .general{
+  color: white;
+  background-color: #28a745;
+  padding: 5px;
+}
+aside .info{
+  padding-left: 15px;
+}
+#mapid{
+  margin-top: 2px;
+  height: 800px;
+  width: 100%;
+}
+.fa-arrow-left{
+  color: green;
+  padding: 10px;
+}
+#error_zoom{
+  color: white;
+  width: 100%;
+  height: 50px;
+  text-align: center;
+  background-color: #778899;
+  margin-top: 0px;
+  margin-left: auto;
+  margin-right: auto;
+  display: hidden;
+}
+</style>
 
 </head>
 @if(isset($latitud)&&isset($longitud))
-  <body  onload="getAseos2({{$latitud}}, {{$longitud}})">
-@else
+<body  onload="getAseos2({{$latitud}}, {{$longitud}})">
+  @else
   <body>
-@endif
-  @if(Session::has('status'))
-  <div id="alert" class="alert {{ Session::get('alert-class', 'alert-warning') }}"><div>{{ Session::get('status') }}</div><i class="fas fa-times" onclick="cerrar()"></i></div>
-  @endif
-  <div class="container-fluid">
-  <div class="row">
-    <aside id="aside" class="col-md-3" hidden>
-      <div class="atras" onclick="volver()">
-        <i class="fas fa-arrow-left fa-2x"></i>
-      </div>
-      <img alt="Imagen no disponible" id="imgWC">
-      <div class="general">
-        <h1 id="nombre"></h1>
-        <p>Puntuacion: <b id="puntuacion"></b></p>
-        <p id="dir"></p>
-        <!-- <form method="post" action="ficha" enctype="multipart/form-data">
-          @csrf
-          <div class="form-group">
+    @endif
+    @if(Session::has('status'))
+    <div id="alert" class="alert {{ Session::get('alert-class', 'alert-warning') }}"><div>{{ Session::get('status') }}</div><i id="x" class="fas fa-times"></i></div>
+    @endif
+    <div class="container-fluid">
+      <div class="row">
+        <aside id="aside" class="col-md-3" hidden>
+          <div class="atras" onclick="volver()">
+            <i class="fas fa-arrow-left fa-2x"></i>
+          </div>
+          <img alt="Imagen no disponible" id="imgWC">
+          <div class="general">
+            <h1 id="nombre"></h1>
+            <p>Puntuacion: <b id="puntuacion"></b></p>
+            <p id="dir"></p>
+            <!-- <form method="post" action="ficha" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
             <input type="file" class="form-control" name="foto">
           </div>
         </form> -->
@@ -134,33 +122,29 @@
       </div>
     </section>
   </div>
-  </div>
-  @include('includes.geoscripts')
-  <script src="/js/map.js"></script>
-  <script type="text/javascript">
-            function valorar(n){
-              for(var i=1; i<=5; i++){
-                var estrella=document.getElementById('estrella'+i);
-                if(i>n){
-                  estrella.classList.add("far");
-                  estrella.classList.remove("fas");
-                }else{
-                  estrella.classList.remove("far");
-                  estrella.classList.add("fas");
-                }
-              }
-            }
+</div>
+@include('includes.geoscripts')
+<script src="/js/map.js"></script>
+<script type="text/javascript">
+  function valorar(n){
+    for(var i=1; i<=5; i++){
+      var estrella=document.getElementById('estrella'+i);
+      if(i>n){
+        estrella.classList.add("far");
+        estrella.classList.remove("fas");
+      }else{
+        estrella.classList.remove("far");
+        estrella.classList.add("fas");
+      }
+    }
+  }
 
-            function volver(){
-                var mapaSection = document.getElementById('section');
-                var aside = document.getElementById('aside');
-                mapaSection.classList.remove('col-md-9');
-                mapaSection.classList.add('col-md-12');
-                aside.hidden = true;
-            }
-
-            function cerrar(){
-              document.getElementById("alert").remove();
-            }
-  </script>
-  @endsection
+  function volver(){
+    var mapaSection = document.getElementById('section');
+    var aside = document.getElementById('aside');
+    mapaSection.classList.remove('col-md-9');
+    mapaSection.classList.add('col-md-12');
+    aside.hidden = true;
+  }
+</script>
+@endsection
