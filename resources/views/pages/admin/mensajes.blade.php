08b7b19c44a77c9d92cd16d46a0c22620f784c30
yaimsc <yaimsc@gmail.com>
2019-01-31 08:47:45 +0100

    fixed table

--- resources/views/pages/admin/mensajes.blade.php
+++ resources/views/pages/admin/mensajes.blade.php
@@ -12,33 +12,35 @@
       <input type="submit" value="Filtrar" class="btn btn-success">
     </form>
   </div>
-	<table class="table">
-		<tr>
-			<th>Id</th>
-			<th>Nombre</th>
-			<th>Email</th>
-			<th>Fecha</th>
-			<th>Mensaje</th>
-			<th>Opciones</th>
-		</tr>
-		@foreach($mensajes as $m)
-		<tr>
-			<td>{{$m->id}}</td>
-			<td>{{$m->name}}</td>
-			<td>{{$m->email}}</td>
-			<td>{{$m->created_at}}</td>
-			<td>{{$m->message}}</td>
-			<td>
-				<a href="mailto:{{$m->email}}?Subject=Respuesta%20de%20WCSearch" class="btn btn-primary">Responder</a>
-				<a href="{{route('admin.mensaje.eliminar',$m->id)}}" class="btn btn-danger">Eliminar</a>
-			</td>
-		</tr>
-		@endforeach
-		@if($mensajes->count()==0)
-			<tr>
-				<td colspan="6"> No hay mensajes </td>
-			</tr>
-		@endif
-	</table>
+  <div class="table-responsive">
+    <table class="table">
+  		<tr>
+  			<th>Id</th>
+  			<th>Nombre</th>
+  			<th>Email</th>
+  			<th>Fecha</th>
+  			<th>Mensaje</th>
+  			<th>Opciones</th>
+  		</tr>
+  		@foreach($mensajes as $m)
+  		<tr>
+  			<td>{{$m->id}}</td>
+  			<td>{{$m->name}}</td>
+  			<td>{{$m->email}}</td>
+  			<td>{{$m->created_at}}</td>
+  			<td>{{$m->message}}</td>
+  			<td>
+  				<a href="mailto:{{$m->email}}?Subject=Respuesta%20de%20WCSearch" class="btn btn-primary">Responder</a>
+  				<a href="{{route('admin.mensaje.eliminar',$m->id)}}" class="btn btn-danger">Eliminar</a>
+  			</td>
+  		</tr>
+  		@endforeach
+  		@if($mensajes->count()==0)
+  			<tr>
+  				<td colspan="6"> No hay mensajes </td>
+  			</tr>
+  		@endif
+  	</table>
+  </div>
 </div>
 @endsection
--- resources/views/pages/admin/usuarios.blade.php
+++ resources/views/pages/admin/usuarios.blade.php
@@ -21,53 +21,52 @@
       <input type="submit" value="Filtrar" class="btn btn-success">
     </form>
   </div>
-	<table class="table">
-		<tr>
-			<th>Id</th>
-			<th>Nombre</th>
-			<th>Email</th>
-			<th>Rol</th>
-			<th>Puntuacion</th>
-			<th>Reportes</th>
-			@if($baneados==true)
-				<th>Fecha de baneo</th>
-			@endif
-			<th>Opciones</th>
-		</tr>
-		@foreach($usuarios as $u)
-		<tr>
-			<td>{{$u->id}}</td>
-			<td>{{$u->name}}</td>
-			<td>{{$u->email}} <b>{{$u->email_verified_at==null?"(sin verificar)":""}}</b></td>
-			<td>{{$u->role->nombre}}</td>
-			<td>{{$u->puntuacion}}</td>
-			<td>{{$u->reportes}}</td>
-			<td>
-			@if($baneados==true)
-				{{$u->fecha_de_baneo}}
-			</td>
-			<td>
+  <div class="table-responsive">
+    <table class="table">
+  		<tr>
+  			<th>Id</th>
+  			<th>Nombre</th>
+  			<th>Email</th>
+  			<th>Rol</th>
+  			<th>Puntuacion</th>
+  			<th>Reportes</th>
+  			@if($baneados==true)
+  				<th>Fecha de baneo</th>
+  			@endif
+  			<th>Opciones</th>
+  		</tr>
+  		@foreach($usuarios as $u)
+  		<tr>
+  			<td>{{$u->id}}</td>
+  			<td>{{$u->name}}</td>
+  			<td>{{$u->email}} <b>{{$u->email_verified_at==null?"(sin verificar)":""}}</b></td>
+  			<td>{{$u->role->nombre}}</td>
+  			<td>{{$u->puntuacion}}</td>
+  			<td>{{$u->reportes}}</td>
+  			<td>
+  			@if($baneados==true)
+  				{{$u->fecha_de_baneo}}
+  			</td>
+  			<td>
 
-				<a href="{{route('admin.usuario.desbanear',$u->id)}}" class="btn btn-danger">Desbanear</a>
-			@else
-				<a href="{{route('admin.usuario.banear',$u->id)}}" class="btn btn-danger">Banear</a>
+  				<a href="{{route('admin.usuario.desbanear',$u->id)}}" class="btn btn-danger">Desbanear</a>
+  			@else
+  				<a href="{{route('admin.usuario.banear',$u->id)}}" class="btn btn-danger">Banear</a>
 
-			@endif
-				@if($u->email_verified_at==null)
-					<a href="{{route('admin.usuario.validar',$u->id)}}" class="btn btn-secondary">Validar</a>
-				@endif
-				<a href="{{route('admin.usuario.editar',$u->id)}}" class="btn btn-primary">Editar</a>
-			</td>
-		</tr>
-		@endforeach
-		@if($usuarios->count()==0)
-			<tr>
-				<td colspan="{{$baneados?8:7}}"> No hay usuarios </td>
-			</tr>
-		@endif
-	</table>
-
-
-
+  			@endif
+  				@if($u->email_verified_at==null)
+  					<a href="{{route('admin.usuario.validar',$u->id)}}" class="btn btn-secondary">Validar</a>
+  				@endif
+  				<a href="{{route('admin.usuario.editar',$u->id)}}" class="btn btn-primary">Editar</a>
+  			</td>
+  		</tr>
+  		@endforeach
+  		@if($usuarios->count()==0)
+  			<tr>
+  				<td colspan="{{$baneados?8:7}}"> No hay usuarios </td>
+  			</tr>
+  		@endif
+  	</table>
+  </div>
 </div>
 @endsection
