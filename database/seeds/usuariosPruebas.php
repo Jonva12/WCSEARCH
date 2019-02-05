1c8ce1598f4f33e70e3be85ce4afba24a9bd0108
jonva12 <jonva12@hotmail.com>
2019-02-05 04:59:33 +0100

    Cambio seeder usuariosPrueba

--- database/seeds/usuariosPruebas.php
+++ database/seeds/usuariosPruebas.php
@@ -12,13 +12,22 @@ class usuariosPruebas extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Prueba',
-            'email' => 'prueba@gmail.com',
+            'name' => 'Patxi',
+            'email' => 'patxi@gmail.com',
             'password' => '$2y$10$WAIlqJoaSMLgpG.FlXSzzOb.Xm54T/LSbGWvSyKTdZtP3j/Y92MYy',
             'puntuacion' => random_int(1,100),
             'reportes' => 0,
             'email_verified_at' => '2019-01-10 09:52:37',
             'role_id' => '1',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Ander',
+            'email' => 'Ander@gmail.com',
+            'password' => '$2y$10$WAIlqJoaSMLgpG.FlXSzzOb.Xm54T/LSbGWvSyKTdZtP3j/Y92MYy',
+            'puntuacion' => random_int(1,100),
+            'reportes' => 0,
+            'email_verified_at' => '2019-01-10 09:52:37',
+            'role_id' => '2',
         ]);
         DB::table('users')->insert([
             'name' => 'Admin',
