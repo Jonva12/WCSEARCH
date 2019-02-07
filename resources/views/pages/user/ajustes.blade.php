6b09633d707eb7eec0bb13873fa8565d06dae550
yaimsc <yaimsc@gmail.com>
2019-02-07 10:35:05 +0100

    ajustes

--- resources/views/pages/user/ajustes.blade.php
+++ resources/views/pages/user/ajustes.blade.php
@@ -3,10 +3,21 @@
   @section('title', 'WCSearch')
 
   @section('content')
+  <style media="screen">
+    .editar, .cambiar, .eliminar{
+      padding: 20px;
+      border-bottom: 1px solid grey;
+    }
+    h1{
+      text-align:center;
+      padding-top: 20px;
+      border-bottom: 1px solid grey;
+    }
+
+  </style>
   @if(Session::has('status'))
     <div id="alert" class="alert {{ Session::get('alert-class', 'alert-warning') }}"><div>{{ Session::get('status') }}</div><i id="x" class="fas fa-times" onclick="cerrar()"></i></div>
   @endif
-	<div class="container">
 		<h1>Ajustes de usuario</h1>
 		@if ($errors->any())
               <div class="alert alert-danger">
@@ -17,38 +28,45 @@
                   </ul>
               </div>
           @endif
-		<h2>Cambiar Perfil</h2>
-		<form action="{{route('usuario.cambiarDatos')}}" >
-			<p><strong>Nombre:</strong> <input type="text" value="{{Auth::user()->name}}" name="nombre"></p>
-			<p><strong>Email:</strong> <input type="text" value="{{Auth::user()->email}}" name="email"></p>
-			<input type="submit" value="Cambiar">
-		</form>
-		<h2>Cambiar contraseña</h2>
-		<form action="{{route('usuario.cambiarPassword')}}" >
-			<p><strong>Contraseña actual:</strong> <input type="password" name="passwordActual"></p>
-			<p><strong>Nueva contraseña:</strong> <input type="password" name="passwordNueva" id="passwordNueva"></p>
-			<p><strong>Repite la nueva contraseña:</strong> <input type="password" name="passwordNueva2" id="passwordNueva2" oninput="comporbarPassword()"></p>
-			<script type="text/javascript">
-			function comporbarPassword(){
-                  var pass= document.getElementById('passwordNueva').value;
-                  var pass2= document.getElementById('passwordNueva2');
-                  var submit= document.getElementById('passwordSubmit');
-                  if (pass==pass2.value){
-                    pass2.classList.remove("is-invalid");
-                    submit.disabled = false;
-                  }else{
-                    pass2.classList.add("is-invalid");
-                    submit.disabled = true;
-                  }
-                }
-                </script>
-			<input type="submit" value="Cambiar" id="passwordSubmit">
-		</form>
-		<h1>Borrar Cuenta</h1>
-		<form action="{{route('usuario.borrarCuenta')}}" >
-			<p><strong>Contraseña actual:</strong> <input type="password" name="password"><input type="submit" value="Borrar cuenta" class="btn btn-danger"></p>
-		</form>
 
-
+    <div class="container">
+      <div class="editar">
+        <h2>Editar Perfil</h2>
+    		<form action="{{route('usuario.cambiarDatos')}}" >
+    			<p>Nombre:<input type="text" class="form-control" value="{{Auth::user()->name}}" name="nombre"></p>
+    			<p>Email:<input type="text" class="form-control" value="{{Auth::user()->email}}" name="email"></p>
+    			<input type="submit" value="Editar" class="btn btn-success">
+    		</form>
+      </div>
+      <div class="cambiar">
+        <h2>Cambiar contraseña</h2>
+    		<form action="{{route('usuario.cambiarPassword')}}" >
+    			<p>Contraseña actual: <input type="password" class="form-control" name="passwordActual"></p>
+    			<p>Nueva contraseña:<input type="password" class="form-control" name="passwordNueva" id="passwordNueva"></p>
+    			<p>Repite la nueva contraseña:<input type="password" class="form-control" name="passwordNueva2" id="passwordNueva2" oninput="comporbarPassword()"></p>
+    			<script type="text/javascript">
+    			function comporbarPassword(){
+                      var pass= document.getElementById('passwordNueva').value;
+                      var pass2= document.getElementById('passwordNueva2');
+                      var submit= document.getElementById('passwordSubmit');
+                      if (pass==pass2.value){
+                        pass2.classList.remove("is-invalid");
+                        submit.disabled = false;
+                      }else{
+                        pass2.classList.add("is-invalid");
+                        submit.disabled = true;
+                      }
+                    }
+                    </script>
+    			<input type="submit" value="Cambiar" id="passwordSubmit" class="btn btn-success">
+    		</form>
+      </div>
+      <div class="eliminar">
+        <h2>Eliminar Cuenta</h2>
+    		<form action="{{route('usuario.borrarCuenta')}}" >
+    			<p>Contraseña actual: <input type="password" class="form-control" name="password"></p>
+          <input type="submit" value="Borrar cuenta" class="btn btn-danger">
+    		</form>
+      </div>
 	</div>
 	@endsection
