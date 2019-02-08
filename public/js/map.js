ae36b9aa4920dcc637568ac6cbfc75f29dba5423
jartolair <jartolair17dw@ikzubirimanteo.com>
2019-02-08 10:58:28 +0100

    marcker anchor konpondute

--- public/js/map.js
+++ public/js/map.js
@@ -89,7 +89,7 @@ var aseoIcon = L.icon({
 
     iconSize:     [25, 35], // size of the icon
     shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 35], // point of the icon which will correspond to marker's location
+	iconAnchor:   [12, 35], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
