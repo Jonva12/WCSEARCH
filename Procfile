9eb953e99965267623c050ae6afc5f8295e4115a
Yaiza <yaimsc@gmail.com>
2019-02-21 23:23:42 +0100

    Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-
-web: vendor/bin/heroku-php-apache2 -F .user.ini public/
+web: vendor/bin/heroku-php-apache2 public/
--- public/index.php
+++ public/index.php
@@ -49,6 +49,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+ ini_set('post_max_size', '100M'); ini_set('upload_max_filesize', '100M'); ini_set('memory_limit', '1000M'); ini_set('max_execution_time', '1920');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
